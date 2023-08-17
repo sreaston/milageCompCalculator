46321d9c6a5788e8ecf1c9664b89dbb3dc6ac4fc
sreaston <eastonspencer11@gmail.com>
2023-08-17 00:27:54 -0500

    Moving program to GUI
    The program now takes input through a GUI. Its output is still console
    based but I am working on making it appear below the Submit button.

--- milageCompCalculator/src/milageCompCalculator/Main.java
+++ milageCompCalculator/src/milageCompCalculator/Main.java
@@ -1,52 +1,90 @@
 package milageCompCalculator;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
-public class Main {
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+public class Main extends JFrame implements ActionListener {
+	
+	private JTextField carMPGField;
+	private JTextField gasPriceField;
+	private JTextField distanceField;
+	private JTextField compensationRateField;
+	
+	private JButton button;
+	
+	public Main() {
+		setTitle("Milage Pay Calculator");
+        setSize(300, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        // Create the first TextField
+        carMPGField = new JTextField(20);
+        add(new JLabel("Car MPG:"));
+        add(carMPGField);
+
+        // Create the second TextField
+        gasPriceField = new JTextField(20);
+        add(new JLabel("Gas price:"));
+        add(gasPriceField);
+
+        distanceField = new JTextField(20);
+        add(new JLabel("Miles driven:"));
+        add(distanceField);
+
+        compensationRateField = new JTextField(20);
+        add(new JLabel("Pay per mile:"));
+        add(compensationRateField);
+        
+        // Create the Button
+        button = new JButton("Submit");
+        button.addActionListener(this);
+        add(button);
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		 if (e.getSource() == button) {
+
+			 	double compensation;
+			 	double gallonsUsed;
+			 	double tripCostGross;
+			 	double profit;
+	            double carMPG = Double.parseDouble(carMPGField.getText());
+	            double gasPrice = Double.parseDouble(gasPriceField.getText());
+	            double distance = Double.parseDouble(distanceField.getText());
+	            double compensationRate = Double.parseDouble(compensationRateField.getText());
+	            
+	            gallonsUsed = (distance / carMPG);
+				tripCostGross = (gallonsUsed * gasPrice);
+				compensation = (compensationRate * distance);
+				profit = (compensation - tripCostGross);
+		 
+		 }
+		 
+	    
+	
+	}
 		
 	public static void main(String args[]) { 	 
+		 SwingUtilities.invokeLater(() -> {
+	            Main example = new Main();
+	            example.setVisible(true);
+	        });
+		
+	
 		
 		
-			
-			double carMPG;
-			double gasPrice;
-			double distance;
-			double compensation;
-			double compensationRate;
-			double profit;
-			double gallonsUsed;
-			double totalCost;
-			double tripCostGross;
-			double tripCostNet;
-			String answer;
-			do {
-			System.out.println("Enter the MPG of your car:");
-			Scanner scnr = new Scanner(System.in);
-			carMPG = scnr.nextFloat();
-			System.out.println("Enter the price of gas:");
-			gasPrice = scnr.nextFloat();
-			System.out.println("Enter the driving distance:");
-			distance = scnr.nextFloat();
-			System.out.println("Enter how much you are paid per mile:");
-			compensationRate = scnr.nextFloat();
-			
-			gallonsUsed = (distance / carMPG);
-			tripCostGross = (gallonsUsed * gasPrice);
-			compensation = (compensationRate * distance);
-			profit = (compensation - tripCostGross);
-			
-			System.out.println("======================================================================");
-			System.out.println("Gallons used to drive this distance (avg): " + gallonsUsed);
-			System.out.printf("Cost to drive this distance: $%.2f \n", tripCostGross);
-			System.out.printf("Compensation before paying for gas: $%.2f \n ", compensation);
-			System.out.printf("Profit from drive: $%.2f \n", profit);
-			System.out.println("======================================================================");
-			System.out.println("Do you wish to calculate another trip? Y/N");
-			answer = scnr.next();
-			}
-			while (answer.equals("Y"));
-			
-			
-			
 		
-	}
+	
+}
 
 }
